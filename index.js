efb6ed88827306aad80a4dfd78814a034334d643
Joseph <josephylee72@gmail.com>
2023-09-15 00:30:34 -0700

    completed sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(username, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: username,
+      email: email
+    })
+  })
+  .then(res => res.json())
+  .then(json => {
+    const id = json.id;
+    const newId = document.createElement('p');
+    newId.textContent = id;
+    document.body.append(newId);
+  })
+  .catch(error => document.body.append(error.message));
+}
+
+submitData('joeylee08', 'joeylee08@gmail.com')
+submitData('joeylee08', 'joeylee08@gmail.com')
+submitData('joeylee08', 'joeylee08@gmail.com')
+submitData('joeylee08', 'joeylee08@gmail.com')
+submitData('joeylee08', 'joeylee08@gmail.com')
+submitData('joeylee08', 'joeylee08@gmail.com')
